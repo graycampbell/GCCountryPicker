5c7cf9667d884a8ef32ec973e17d30dff6cb7fea
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2017-09-28 15:37:05 -0500

    Refactor

--- GCCountryPickerDemo/ViewController.swift
+++ GCCountryPickerDemo/ViewController.swift
@@ -2,24 +2,12 @@
 //  ViewController.swift
 //  GCCountryPickerDemo
 //
-//  Created by MacBook Pro on 9/28/17.
-//  Copyright © 2017 Gray Campbell. All rights reserved.
+//  Created by Gray Campbell on 9/28/17.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
