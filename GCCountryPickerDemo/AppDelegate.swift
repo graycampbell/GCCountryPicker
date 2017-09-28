d857b21a3a1bd32c9994c97d11068cca1a7e2ca1
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2017-09-28 18:56:50 -0500

    Update pragma marks

--- GCCountryPickerDemo/AppDelegate.swift
+++ GCCountryPickerDemo/AppDelegate.swift
@@ -11,7 +11,11 @@ import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    // MARK: - Properties
+    
     var window: UIWindow?
+    
+    // MARK: - Application
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
